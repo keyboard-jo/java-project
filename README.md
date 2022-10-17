167b4034366aa5959fc0e4b0acd764864a0d32b0
justin <justin.sulaiman@gmail.com>
2022-10-17 12:02:43 +0800

    Add features GUI

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# java-project
+# Java-Project-OODJ
+This repository contains files that program a car rental service with GUI
 
-OODJ project
+edit 1
--- src/caroodj/Swing.java
+++ src/caroodj/Swing.java
@@ -3,13 +3,13 @@ import javax.swing.*;
 public class Swing {  
     JFrame f;  
     public Swing(){  
-        f = new JFrame();//creating instance of JFrame  
-        JButton b = new JButton("click");//creating instance of JButton  
+        f = new JFrame(); //creating instance of JFrame  
+        JButton b = new JButton("click"); //creating instance of JButton  
         b.setBounds(130,100,100, 40);  
           
-        f.add(b);//adding button in JFrame  
+        f.add(b); //adding button in JFrame  
           
-        f.setSize(400,500);//400 width and 500 height  
-        f.setLayout(null);//using no layout managers  
-        f.setVisible(true);//making the frame visible  
-    }}  
+        f.setSize(400,500); //400 width and 500 height  
+        f.setLayout(null); //using no layout managers  
+        f.setVisible(true); //making the frame visible  
+    }}
